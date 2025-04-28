add675e56aa6200475841538221a198f4b09b76f
sudharsan.parthasarathi <sudharsan.parthasarathi@moneyview.in>
2025-04-28 10:55:00 +0530

    Fix: Use correct case for end.MP3 audio file and stage all assets for deployment

---
+++ src/components/StickyFooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StickyFooter.css';
+
+const StickyFooter = () => (
+  <div className="sticky-footer">
+    <img src={require('../../assets/bottom.png')} alt="footer" className="footer-img" />
+  </div>
+);
+
+export default StickyFooter; 
